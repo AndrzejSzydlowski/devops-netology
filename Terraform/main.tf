7f14dc665173f878f4878e29a9fc511d2872be34
rimm <adrzejsz@gmail.com>
2021-04-05 22:08:33 +0300

    terraform1.2

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,29 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  access_key = $AWS_ACCESS_KEY_ID
+  secret_key = $AWS_SECRET_ACCESS_KEY
 }
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
